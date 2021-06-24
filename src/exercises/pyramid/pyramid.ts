1124c4d47deb925e26862f185c15855b493263e4
Medz <45122755+medzz123@users.noreply.github.com>
2021-06-24 08:15:49 +0000

    remove recursion to do it again

--- src/exercises/pyramid/pyramid.ts
+++ src/exercises/pyramid/pyramid.ts
@@ -14,27 +14,6 @@
 //       ' ### '
 //       '#####'
 
-export const pyramidOne = (n, row = 0, level = '') => {
-  if (row === n) {
-    return n;
-  }
-
-  if (level.length === 2 * n - 1) {
-    console.log(level);
-    return pyramidOne(n, row + 1);
-  }
-
-  const mid = Math.floor((2 * n - 1) / 2);
-
-  let add = ' ';
-
-  if (mid - row <= level.length && mid + row >= level.length) {
-    add = '#';
-  }
-
-  pyramidOne(n, row, level + add);
-};
-
 export const pyramidTwo = (n: number) => {
   const total = 2 * n - 1;
 
